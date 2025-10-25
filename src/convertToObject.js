d844c6dee1f2a553a2139d22df2d3a4db04f70fe
MiltonMario <miltonneto0010@gmail.com>
2025-10-25 12:32:22 -0300

    Add my solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const source = sourceString
+    .split(';')
+    .map((item) => item.replace('\n', '').trim())
+    .filter((item) => item.includes(':'));
+
+  for (const word of source) {
+    const parts = word.split(':');
+    const chave = parts[0].trim();
+    const valor = parts[1].trim();
+
+    result[chave] = valor;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
